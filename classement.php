281ef7c4d9a11d37de0f732e7b69636ae933127b
Ralsywy <121875489+Ralsywy@users.noreply.github.com>
2023-03-07 14:10:22 +0100

    classement vitrine (à changer)

---
+++ classement.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Queasy</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='style.css'>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+</head>
+<body style="background-color: #648BC4">
+    <img src="A.png" class="logo">
+    <h1 class="classement">Votre Classement</h1>
+    <p class="moi">Vous êtes 4ème (16 points)</p>
+    <p class="premier">Florian Vauthier | 20 points</p>
+    <p class="deuxieme">Luca Haser | 18 points</p>
+    <p class="troisieme">George Malin | 17 points</p>
+    <p class="reste">Reste du classement</p>
+    <p class="quatre">4. Ethan Delbos | 16 points</p>
+    <p class="cinq">5. Jerry Can | 14 points</p>
+    <p class="six">6. Edgar Atoi | 12 points</p>
+    <p class="sept">7. Ella de Bonzieux | 5 points</p>
+
+    <img src="podium.png" class="podium">
+</body>
+</html>
