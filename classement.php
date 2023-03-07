91ed3f32ab9abd1f4875b720f9e04bd4a44842d0
Ralsywy <121875489+Ralsywy@users.noreply.github.com>
2023-03-07 14:13:41 +0100

    correction

--- classement.php
+++ classement.php
@@ -9,7 +9,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
 </head>
 <body style="background-color: #648BC4">
-    <img src="A.png" class="logo">
+    <img src="Logo-QUEASY.png" class="logo">
     <h1 class="classement">Votre Classement</h1>
     <p class="moi">Vous êtes 4ème (16 points)</p>
     <p class="premier">Florian Vauthier | 20 points</p>
