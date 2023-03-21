da90f83212dfdbf99010d35131b6b94457871ceb
Ralsywy <bichonkevin4@gmail.com>
2023-03-21 14:05:11 +0100

    Accès à la base de donnée

--- classement.php
+++ classement.php
@@ -24,3 +24,18 @@
     <img src="podium.png" class="podium">
 </body>
 </html>
+
+<?php // Accès à la base de donnée
+     $mysqlConnection = new PDO( 
+
+        'mysql:host="https://www.db4free.net/phpMyAdmin/index.php?route=/&route=%2F&db=queasy";dbname=queasy;charset=utf8', 
+  
+        'florian', 
+  
+        'Flori@n1512' );
+?>
+
+<?php           
+
+            // Execution de la requête 
+            
