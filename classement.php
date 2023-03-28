9c522b4a1ef31ab34c24ce63c5d01ec7925c7d35
Ralsywy <121875489+Ralsywy@users.noreply.github.com>
2023-03-28 14:57:08 +0200

    Ajout header dans classement.php

--- classement.php
+++ classement.php
@@ -10,7 +10,9 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
 </head>
 <body style="background-color: #648BC4">
-    <img src="Logo-QUEASY.png" class="logo">
+<?php
+include('Quesay espace eleve/Espace Admin/headerutilisateur.php');
+?>
     <h1 class="classement">Votre Classement</h1>
 
     <p class="reste">Reste du classement</p>
